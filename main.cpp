803d98233c53cdf419b0a1d999932a3a865bb7e7
CamiDoffo <116676956+CamiDoffo@users.noreply.github.com>
2022-10-25 16:18:46 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ int main(int argc, char *argv[]) {
 	
 	// show the month of maximum rainfall per year
 	t_month output2[YEARS];
-	max_monthly_rainfall_per_year(array, output2);*/
+	max_monthly_rainfall_per_year(array, output2);
 	return 0;
 }
 
